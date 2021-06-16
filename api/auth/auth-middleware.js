73e76898d25a47704e2cbc26ba50bad932477a2a
Your Name <you@example.com>
2021-06-16 06:27:40 +0100

    formatting of codes

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,56 +1,24 @@
 const { JWT_SECRET } = require("../secrets"); // use this secret!
 const jwt = require('jsonwebtoken')
-const {findBy} = require("../users/users-model")
+const { findBy } = require("../users/users-model")
 
 const restricted = (req, res, next) => {
-  /*
-    If the user does not provide a token in the Authorization header:
-    status 401
-    {
-      "message": "Token required"
-    }
-
-    If the provided token does not verify:
-    status 401
-    {
-      "message": "Token invalid"
-    }
-
-    Put the decoded token in the req object, to make life easier for middlewares downstream!
-  */
   const token = req.headers.authorization
   if (!token) {
-    return next ({ status: 401, message: `Token required`})
+    return next({ status: 401, message: `Token required` })
   }
   jwt.verify(token, JWT_SECRET, (err, decoded) => {
-      if (err) {
-        next({ status: 401, message: `'Token invalid'` })
-      } else {
-        req.decoded = decoded
-        next()
-      }
-  }) 
-  // else {
-  //   res.status(401).json({
-  //     message: 'Token invalid'
-  //   })
-  // }
+    if (err) {
+      next({ status: 401, message: `'Token invalid'` })
+    } else {
+      req.decoded = decoded
+      next()
+    }
+  })
 
 }
 
 const only = role_name => (req, res, next) => {
-  /*
-    If the user does not provide a token in the Authorization header with a role_name
-    inside its payload matching the role_name passed to this function as its argument:
-    status 403
-    {
-      "message": "This is not for you"
-    }
-
-    Pull the decoded token from the req object, to avoid verifying it again!
-  */
-
-
   if (role_name === req.decoded.role_name) {
     next()
   } else {
@@ -62,15 +30,8 @@ const only = role_name => (req, res, next) => {
 
 
 const checkUsernameExists = async (req, res, next) => {
-  /*
-    If the username in req.body does NOT exist in the database
-    status 401
-    {
-      "message": "Invalid credentials"  
-    }
-  */
   try {
-    const [ user ] = await findBy({ username: req.body.username })
+    const [user] = await findBy({ username: req.body.username })
     if (!user) {
       next({ status: 401, message: `Invalid credentials` })
     } else {
@@ -84,39 +45,21 @@ const checkUsernameExists = async (req, res, next) => {
 
 
 const validateRoleName = (req, res, next) => {
-  /*
-    If the role_name in the body is valid, set req.role_name to be the trimmed string and proceed.
 
-    If role_name is missing from req.body, or if after trimming it is just an empty string,
-    set req.role_name to be 'student' and allow the request to proceed.
-
-    If role_name is 'admin' after trimming the string:
-    status 422
-    {
-      "message": "Role name can not be admin"
-    }
-
-    If role_name is over 32 characters after trimming the string:
-    status 422
-    {
-      "message": "Role name can not be longer than 32 chars"
-    }
-  */
   if (!req.body.role_name || req.body.role_name.trim()) {
     req.role_name = 'student'
     next()
   }
   else if (req.body.role_name.trim() === 'admin') {
-    next({ status: 422, message: 'Role name can not be admin'})
-  } 
-  else if (req.body.role_name.trim().length > 32 ) {
-    next({ status: 422, message: 'Role name can not be longer than 32 chars'})
+    next({ status: 422, message: 'Role name can not be admin' })
   }
-   else {
+  else if (req.body.role_name.trim().length > 32) {
+    next({ status: 422, message: 'Role name can not be longer than 32 chars' })
+  }
+  else {
     req.role_name = req.body.role_name.trim()
     next()
   }
-
 }
 
 module.exports = {
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -2,22 +2,7 @@ const router = require("express").Router();
 const Users = require("./users-model.js");
 const { restricted, only } = require("../auth/auth-middleware.js");
 
-/**
-  [GET] /api/users
-
-  This endpoint is RESTRICTED: only authenticated clients
-  should have access.
-
-  response:
-  status 200
-  [
-    {
-      "user_id": 1,
-      "username": "bob"
-    }
-  ]
- */
-router.get("/", restricted, (req, res, next) => { // done for you
+router.get("/", restricted, (req, res, next) => { 
   Users.find()
     .then(users => {
       res.json(users);
@@ -25,22 +10,7 @@ router.get("/", restricted, (req, res, next) => { // done for you
     .catch(next);
 });
 
-/**
-  [GET] /api/users/:user_id
-
-  This endpoint is RESTRICTED: only authenticated users with role 'admin'
-  should have access.
-
-  response:
-  status 200
-  [
-    {
-      "user_id": 1,
-      "username": "bob"
-    }
-  ]
- */
-router.get("/:user_id", restricted, only('admin'), (req, res, next) => { // done for you
+router.get("/:user_id", restricted, only('admin'), (req, res, next) => { 
   Users.findById(req.params.user_id)
     .then(user => {
       res.json(user);
